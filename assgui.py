be59d95438e37ddfda9b1c2633441f0c05d42b76
Bence Orosz <oroszbence0929@gmail.com>
2020-06-21 00:58:07 +0200

    setAreaGui

--- assgui.py
+++ assgui.py
@@ -10,10 +10,11 @@ from gmath import GraphicsMath
 class ASSGUI:
 
     def __init__(self, master, img):
+        self.img = img
         self.master = master
-        self.label = tk.Label(self.master, text="Click me")
-        self.label.pack()
-        self.label.bind("<Button>", self.mouseClick)
+
+        self.createButton("SetScanArea", 0, 0, self.ScanAreaClick)
+
         self.canvas = tk.Canvas(self.master, width=0, height=0)
         self.pilImage = np.array(img)[:, :, ::-1].copy()
 
@@ -22,13 +23,43 @@ class ASSGUI:
         self.image = Image.fromarray(img)
         self.photo = ImageTk.PhotoImage(self.image)
         self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
-        self.canvas.pack()
-        self.canvas.bind("<Button>", self.mouseClick)
+        self.canvas.grid(row=1, column=0, columnspan=5)
+        self.canvas.bind("<Button-1>", self.mouseClickOnCanvas)
 
-    def mouseClick(self, event):
+    def mouseClickOnCanvas(self, event):
         print("mouse clicked at x={0} y={1}".format(event.x, event.y))
         for point in GraphicsMath.findLineIntersections(self.pilImage):
             cv2.circle(self.pilImage, point, 1, (255, 0, 0), 1)
         self.image = Image.fromarray(self.pilImage)
         self.photo = ImageTk.PhotoImage(self.image)
         self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
+
+    def ScanAreaClick(self, event):
+        root = tk.Toplevel()
+        self.newwindow = setAreaGUI(root, self.img)
+        root.mainloop()
+
+    def createButton(self, text, col, row, callback):
+        self.label = tk.Label(self.master, text=text)
+        self.label.grid(row=row, column=col)
+        self.label.bind("<Button-1>", callback)
+
+
+class setAreaGUI(ASSGUI):
+    def __init__(self, master, img):
+        super().__init__(master,img)
+        self.clickcnt=0
+    def mouseClickOnCanvas(self, event):
+        print("mouse clicked at x={0} y={1}".format(event.x, event.y))
+        cv2.circle(self.pilImage, (event.x, event.y), 6, (255, 0, 0), 1)
+        self.image = Image.fromarray(self.pilImage)
+        self.photo = ImageTk.PhotoImage(self.image)
+        self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
+        self.clickcnt+=1
+        if self.clickcnt>=4:
+            CALLBALINT()
+            self.master.destroy()
+
+
+def CALLBALINT():
+    print("Here comes Balint!")
